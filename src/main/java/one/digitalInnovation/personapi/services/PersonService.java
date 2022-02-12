b32e8626c9486d7f3748a84e89c19dac71775fb3
Daniel Soares <danielsoares321@gmail.com>
2022-02-12 13:37:17 -0300

    Delete by id

--- src/main/java/one/digitalInnovation/personapi/services/PersonService.java
+++ src/main/java/one/digitalInnovation/personapi/services/PersonService.java
@@ -1,7 +1,6 @@
 package one.digitalInnovation.personapi.services;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,11 +50,19 @@ public class PersonService {
 
 
 	public PersonDTO findById(Long id) throws PersonNotFoundException {
-		Person person = personRepository.findById(id)
-				.orElseThrow(()->new PersonNotFoundException(id));
-		 
-		
-		
+		Person person = verifyIfExists(id);
 		return personMapper.toDTO(person);
 	}
+	
+	public void delete(Long id) throws PersonNotFoundException {
+		verifyIfExists(id);
+		
+		personRepository.deleteById(id);
+	}
+	
+	public Person verifyIfExists(Long id) throws PersonNotFoundException {
+		return personRepository.findById(id)
+				.orElseThrow(()->new PersonNotFoundException(id));
+	}
+	
 }
